6b35045679c4b117becdeed96867f3ba4a9676e0
apreston95 <36800821+apreston95@users.noreply.github.com>
2023-08-30 12:49:36 +0100

    Add files via upload

---
+++ Practice Session 3/PSIII_Main.m
@@ -0,0 +1,55 @@
+%%% EACBN Summer School 2023, Practice Session III Main File
+%%% Code written by Franck Portier and Andy Preston
+
+clc
+clear all
+close all
+
+
+%% Part 1a -> Model with Production Externalities and Habit Stock
+dynare PSIIIa
+
+clear all
+close all
+
+%% Part 1b -> Model with Production Externalities and Durable Good Stock
+
+dynare PSIIIb1
+
+clear all
+close all
+
+PSIIIb2
+
+clear all
+close all
+
+
+%% Part 1c -> Model with Keeping Up With the Joneses Habits
+
+dynare PSIIIc1
+
+clear all
+close all
+
+dynare PSIIIc2
+
+clear all
+close all
+
+%% Part 1d -> Model with Multiple Features
+
+dynare PSIIId1
+
+clear all
+close all
+
+PSIIId2
+
+clear all
+close all
+
+
+
+
+
